2a7ab7c60c51f8d514b985d4f37f63067c2db626
Shivadubal26 <72974708+Shivadubal26@users.noreply.github.com>
2023-10-23 18:01:07 +0530

    Create Simple_interest.cpp

---
+++ C++/Simple_interest.cpp
@@ -0,0 +1,21 @@
+// C++ Program to Calculate Simple Interest
+#include <iostream>
+using namespace std;
+int main(){
+    int p, t, r, SI;
+    
+    // Asking for input
+    cout << "Enter the principal amount: ";
+    cin >> p;
+    cout << "Enter the time period(in years): ";
+    cin >> t;
+    cout << "Enter the rate of interest: ";
+    cin >> r;
+    
+    // Calculating simple interest
+    SI = (p*r*t) / 100;
+    
+    // Displaying output
+    cout << "Simple Interest: " << SI << endl;
+    return 0;
+}
